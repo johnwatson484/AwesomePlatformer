af621cc095efbb6c7cd430394216edd1a8c46b49
John Watson <john.watson1@defra.gov.uk>
2025-05-08 20:25:05 +0100

    Add player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5f;
+    private Rigidbody2D body;
+    private Animator animator;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +22,25 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        Walk();
+    }
+
+    void Walk()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if(h > 0)
+        {
+            body.linearVelocity = new Vector2(speed, body.linearVelocity.y);
+        }
+        
+        if(h < 0)
+        {
+            body.linearVelocity = new Vector2(-speed, body.linearVelocity.y);
+        }
     }
 }
