40c6e61982d55e58aa8649be218bd41aef776b2d
John Watson <john.watson1@defra.gov.uk>
2025-06-21 18:02:54 +0100

    Smooth movements

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isGrounded;
     private bool jumped;
-    private float jumpPower = 5f;
+    private float jumpPower = 12f;
 
     void Awake()
     {
